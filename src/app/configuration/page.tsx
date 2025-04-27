09eed5d4b811683be4965d5a09a1e34159ce0ded
Mahdi Asadolahzade <mahdiasadi140@gmail.com>
2025-04-27 17:24:59 +0330

    chore

---
+++ src/app/configuration/page.tsx
@@ -0,0 +1,23 @@
+
+import Title from "@/components/typography/Title";
+import React from "react";
+
+
+const page = () => {
+  return (
+    <div>
+      <Title title="Configuration" />
+
+
+<div className="grid grid-cols-2 gap-2">
+    <div className="bg-base2 border-2 border-accent rounded-md">1</div>
+    <div className="bg-base2 border-2 border-accent rounded-md">2</div>
+    <div className="bg-base2 border-2 border-accent rounded-md">3</div>
+    <div className="bg-base2 border-2 border-accent rounded-md">4</div>
+</div>
+  
+    </div>
+  );
+};
+
+export default page;
