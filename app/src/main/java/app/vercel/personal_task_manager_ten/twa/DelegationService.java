f62d2b8db62352b92f56c0067bdeb2e118719cf2
Mahdi Asadolahzade <mahdiasadi140@gmail.com>
2025-05-13 14:08:55 +0330

    stablished android APK

---
+++ app/src/main/java/app/vercel/personal_task_manager_ten/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.personal_task_manager_ten.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
