a66ac5630b2f786a2f4d567d9846d93d560483c7
Mahdi Asadolahzade <mahdiasadi140@gmail.com>
2025-05-19 13:18:10 +0330

    fix lunches

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,11 +2,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'ir.personal.taskmanager',
-  appName: 'personal-task-manager',
-  webDir: 'public',
-
+  appName: 'Personal Task Manager',
+  webDir: 'out',
   server: {
-    url: 'https://personal-task-manager-ten.vercel.app/', // your deployed site
+    url: 'https://personal-task-manager-ten.vercel.app/',
     cleartext: true
   }
 };
