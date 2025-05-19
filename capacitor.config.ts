99e748674cf2a4016176c37a60020458e98c6efb
Mahdi Asadolahzade <mahdiasadi140@gmail.com>
2025-05-19 11:57:12 +0330

    update url

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,9 +4,9 @@ const config: CapacitorConfig = {
   appId: 'ir.personal.taskmanager',
   appName: 'personal-task-manager',
   webDir: 'public',
-  
+
   server: {
-    url: 'https://personal-task-manager.ir', // your deployed site
+    url: 'https://personal-task-manager-ten.vercel.app/', // your deployed site
     cleartext: true
   }
 };
